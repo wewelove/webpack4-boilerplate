9395de11adae29851ab8e1e9cbd7c3d3ecb698be
wewelove <wewelove88@gmail.com>
2020-06-17 10:35:53 +0800

    添加 iconfont 字体

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,13 +1,20 @@
-// 导入样式
+// 导入 Bootstrap 样式
 import 'bootstrap/scss/bootstrap';
+// 导入字体样式，详情参考 fonts 目录示例
+import '@/fonts/iconfont.css';
+import '@/fonts/iconfont.js';
+// 导入公共样式
 import '@/styles/common';
+// 导入页面样式
 import './style';
 
-// 导入脚本, jQuery、Popper  通过全局引入
+// jQuery、Popper 通全局引入，详见配置文件
+// 导入 Bootstrap 库
 import 'bootstrap';
+// 导入公共库
 import '@/scripts/common'
 
 // 页面
 $(function () {
-    console.log('page');
+    console.log('pages/index');
 });
